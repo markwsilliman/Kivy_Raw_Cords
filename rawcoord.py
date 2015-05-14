ebb61fbb5052d9ecf3096ecf7ecc0dc521ccf49b
markwsilliman <mark@smartwaiver.co>
2015-05-14 12:24:21 -0700

    percents

--- rawcoord.py
+++ rawcoord.py
@@ -1,3 +1,6 @@
+#Note: must run as SU e.g. sudo python rawcoord.py
+#To exit: esc then ctrl-c seems to work
+
 import kivy
 kivy.require('1.1.1')
 
@@ -7,17 +10,18 @@ from kivy.properties import NumericProperty, ReferenceListProperty,\
     ObjectProperty
 from kivy.vector import Vector
 from kivy.clock import Clock
+from kivy.core.window import Window
 
 class RawCord(Widget):
     def on_touch_down(self, touch):
 	print "DOWN"
-	print "X: " + str(touch.x)
-	print "Y: " + str(touch.y)
+	print "X: " + str(float(touch.x) / float(Window.size[0]))
+	print "Y: " + str(float(touch.y) / float(Window.size[1]))
 
     def on_touch_up(self, touch):
 	print "UP"
-	print "X: " + str(touch.x)
-	print "Y: " + str(touch.y)
+	print "X: " + str(float(touch.x) / float(Window.size[0]))
+	print "Y: " + str(float(touch.y) / float(Window.size[1]))
 
 
 class RawCordApp(App):
@@ -27,4 +31,5 @@ class RawCordApp(App):
 
 
 if __name__ == '__main__':
+    Window.fullscreen = True
     RawCordApp().run()
