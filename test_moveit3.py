641d9af6eec4ddd08fdde134e1564baf1bcbe1ef
markwsilliman <mark@smartwaiver.co>
2015-06-25 14:21:44 -0700

    table sync

---
+++ test_moveit3.py
@@ -0,0 +1,102 @@
+# Note these must be running!
+# rosrun baxter_interface joint_trajectory_action_server.py
+# roslaunch baxter_moveit_config demo_baxter.launch
+import argparse
+import sys
+import copy
+import rospy
+import numpy
+import cv2
+import tf
+import moveit_commander
+import math
+
+from std_msgs.msg import Header
+
+from moveit_msgs.msg import (
+    AttachedCollisionObject,
+    CollisionObject,
+    PlanningScene,
+    Grasp,
+    GripperTranslation,
+)
+
+from trajectory_msgs.msg import(
+    JointTrajectory,
+    JointTrajectoryPoint
+)
+
+from geometry_msgs.msg import (
+    Point,
+    Polygon,
+    Pose,
+    PoseStamped,
+    Quaternion,
+    Vector3,
+    Vector3Stamped
+)
+from abbe_gripper import Abbe_Gripper
+import baxter_interface
+from baxter_interface import CHECK_VERSION
+import yaml
+
+class testmoveit2:
+    def __init__(self):
+        rospy.init_node("move_test")
+
+        self._grippers = Abbe_Gripper()
+
+        self.robot = moveit_commander.RobotCommander()
+        self.rightHand = moveit_commander.MoveGroupCommander("right_arm")
+        self.scene = moveit_commander.PlanningSceneInterface()
+
+        print "sleeping for 2 seconds"
+        rospy.sleep(2)
+        print "done sleeping"
+
+        p = PoseStamped()
+        p.header.frame_id = self.robot.get_planning_frame()
+        p.pose.position.x = 0.88
+        p.pose.position.y = 0
+        p.pose.position.z = -0.175 - (0.795/2)
+        self.scene.add_box("table", p, (0.72, 1.2, 0.795))
+
+        p = PoseStamped()
+        p.header.frame_id = self.robot.get_planning_frame()
+        p.pose.position.x = 0.88
+        p.pose.position.y = 0
+        p.pose.position.z = -0.175 + (0.095 / 2)
+        self.scene.add_box("cup", p, (0.08, 0.14, 0.095))
+
+        print "Map Done"
+
+
+    def testmove(self):
+        print "Let's Move"
+        self.rightHand.set_goal_position_tolerance(0.02)
+        self.rightHand.set_goal_orientation_tolerance(0.03)
+        self.rightHand.allow_replanning(True)
+
+        quaternion = tf.transformations.quaternion_from_euler(0, math.pi, 0.0)
+
+        poseStamped = PoseStamped()
+        poseStamped.header.frame_id = "base"
+        poseStamped.pose.position.x =  0.35
+        poseStamped.pose.position.y =  -0.46
+        poseStamped.pose.position.z =  0.168
+        poseStamped.pose.orientation.x = quaternion[0]
+        poseStamped.pose.orientation.y = quaternion[1]
+        poseStamped.pose.orientation.z = quaternion[2]
+        poseStamped.pose.orientation.w = quaternion[3]
+
+        self.rightHand.set_pose_target(poseStamped)
+        self.rightHand.go()
+
+        print ( self.rightHand.get_current_pose())
+        print ( self.rightHand.get_current_pose( self.rightHand.get_end_effector_link()))
+
+if __name__ == '__main__':
+    t = testmoveit2()
+
+    #t.testmove()
+    rospy.spin()
